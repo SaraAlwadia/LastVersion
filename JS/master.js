15a02a45e545159afb7d66115fd01294aa9e0e5e
SaraMo7ammd <sara.wadia2016@gmail.com>
2022-03-15 16:15:16 +0200

    first commit

---
+++ JS/master.js
@@ -0,0 +1,53 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function() {
+    /*Active class**************************************************** */
+    // $nav = $('.nav');
+    // $toggleCollapse = $('.toggle-collapse');
+
+    // /**click event on toggle menu */
+    // $toggleCollapse.click(function() {
+    //     $nav.toggleClass('collapse');
+    // });
+    /*Active class**************************************************** */
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        //center: true,
+        nav: true,
+        // navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+});
---
+++ login-form/js/master.js
@@ -0,0 +1,84 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function() {
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /**click event on toggle menu */
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse');
+    });
+
+    // owl-carousel
+    //$('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        //dots: false,
+        //nav: true,
+        // navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+});
+
+// On Click SignUp It Will Hide Login Form and Display Registration Form
+$("#signup").click(function() {
+
+$("#first").slideUp("slow", function() {
+$("#second").slideDown("slow");
+});
+});
+// On Click SignIn It Will Hide Registration Form and Display Login Form
+$("#signin").click(function() {
+$("#second").slideUp("slow", function() {
+$("#first").slideDown("slow");
+});
+});
+
+//*********************************************************************** */
+//Active Class does not work !!!!!!!
+app.plugins.createTab({
+    triggers: '.login-register-form-trigger',
+    elements: '.login-register-form-element',
+    animation: {
+    type: 'slide-in-right'
+},
+onTabChange: function (activeTab) {
+    const firstInput = activeTab.querySelector('input');
+
+    firstInput.focus();
+}
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('#sign button.active').removeClass("active");
+        $(this).addClass("active");
+    });
+});
+//*********************************************************************** */
